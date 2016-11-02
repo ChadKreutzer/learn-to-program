68b5adda826f224ae8c9345447d10c62528fd4e4
Chad Kreutzer <kanthalion@gmail.com>
2016-11-02 09:46:12 +0000

    add verse descriptions to comments in ninety_nine_bottles.rb

--- ch06/ninety_nine_bottles.rb
+++ ch06/ninety_nine_bottles.rb
@@ -1,3 +1,17 @@
 # "99 bottles of beer on the wall..." Write a program which prints
 # out the lyrics to that beloved classic, that field-trip favorite:
 # "99 Bottles of Beer on the Wall."
+#
+# Verses 99 through 2:
+# X bottles of beer on the wall, X bottles of beer.
+# Take one down, pass it around, X - 1 bottles of beer on the wall...
+# If one of those bottles should happen to fall, 98 bottles of beer on the wall...
+#
+# Verse 1:
+# 1 bottle of beer on the wall, 1 bottle of beer.
+# Take it down, pass it around, 0 bottles of beer on the wall...
+# If that bottle should happen to fall, 0 bottles of beer on the wall...
+#
+# Verse 0
+# No more bottles of beer on the wall, no more bottles of beer.
+# Go to the store and buy some more, 99 bottles of beer on the wall...

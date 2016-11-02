6f08a7852a6fe62f13e0ccc18bf30579a656653a
ChadKreutzer <kanthalion@gmail.com>
2016-11-01 21:43:42 -0600

    all but last two challenges done for chapter 01

---
+++ ch01/calc.rb
@@ -0,0 +1,12 @@
+# How many hours are in a year?
+# How many minutes are in decade?
+# How many seconds old are you?
+# How many chocolates do you hope
+#   to eat in your lifetime?
+hours_in_a_year = 365 * 24
+minutes_in_a_decade = hours_in_a_year * 60 * 10
+seconds_old = hours_in_a_year * 60 * 60 * 41
+puts "There are #{hours_in_a_year} hours in a year."
+puts "There are #{minutes_in_a_decade} minutes in a decade."
+puts "I am #{seconds_old} seconds old."
+puts 9.0 / 2.0

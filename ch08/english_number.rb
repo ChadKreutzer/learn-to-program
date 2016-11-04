3f3a655047d0f8dd84c177dc647af249409ebf07
Chad Kreutzer <kanthalion@gmail.com>
2016-11-04 09:11:10 +0000

    englishNumber done through trillions

--- ch08/english_number.rb
+++ ch08/english_number.rb
@@ -1,99 +1,60 @@
 def englishNumber number
-  if number < 0  # No negative numbers.
+  if number < 0
     return 'Please enter a number that isn\'t negative.'
-  elsif number >= 10**9
-    return 'Please enter a smaller number'
   end
   if number == 0
     return 'zero'
   end
 
-  # No more special cases! No more returns!
-
   numString = ''  # This is the string we will return.
 
-  onesPlace = ['one',     'two',       'three',    'four',     'five',
-               'six',     'seven',     'eight',    'nine']
-  tensPlace = ['ten',     'twenty',    'thirty',   'forty',    'fifty',
-               'sixty',   'seventy',   'eighty',   'ninety']
-  teenagers = ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
-               'sixteen', 'seventeen', 'eighteen', 'nineteen']
-  
-  number_size = number.to_s.length
-  if number_size > 6
-    big_number = 'million'
-    places = 10**6
-  elsif number_size > 3
-    big_number = 'thousand'
-    places = 10**3
-  else
-    big_number = 'hundred'
-    places = 10**2
-  end
-  
+  onesPlace =  ['one',     'two',       'three',    'four',     'five',
+                'six',     'seven',     'eight',    'nine']
+  tensPlace =  ['ten',     'twenty',    'thirty',   'forty',    'fifty',
+                'sixty',   'seventy',   'eighty',   'ninety']
+  teenagers =  ['eleven',  'twelve',    'thirteen', 'fourteen', 'fifteen',
+                'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  bigNumbers = [[12, 'trillion'], [9, 'billion'], [6, 'million'],
+                [3, 'thousand'], [2, 'hundred']]
 
   # "left" is how much of the number we still have left to write out.
   # "write" is the part we are writing out right now.
-  # write and left... get it?  :)
   left  = number
-  write = left/places          # How many hundreds left to write out?
-  left  = left - write*places  # Subtract off those hundreds.
-
-  if write > 0
-    # Now here's a really sly trick:
-    hundreds  = englishNumber write
-    numString = numString + hundreds + ' ' + big_number
-    # That's called "recursion". So what did I just do?
-    # I told this method to call itself, but with "write" instead of
-    # "number". Remember that "write" is (at the moment) the number of
-    # hundreds we have to write out. After we add "hundreds" to
-    # "numString", we add the string ' hundred' after it.
-    # So, for example, if we originally called englishNumber with
-    # 1999 (so "number" = 1999), then at this point "write" would
-    # be 19, and "left" would be 99. The laziest thing to do at this
-    # point is to have englishNumber write out the 'nineteen' for us,
-    # then we write out ' hundred', and then the rest of
-    # englishNumber writes out 'ninety-nine'.
-
-    if left > 0
-      # So we don't write 'two hundredfifty-one'...
-      numString = numString + ' '
+  
+  bigNumbers.each do |place|
+    write = left/10**place[0]
+    left  = left - write*10**place[0]
+    if write > 0
+      bigNumber  = englishNumber write
+      numString = numString + bigNumber + ' ' + place[1]
+      if left > 0
+        numString = numString + ' '
+      end
     end
   end
-
-  write = left/10          # How many tens left to write out?
-  left  = left - write*10  # Subtract off those tens.
-
+  
+  write = left/10          
+  left  = left - write*10
   if write > 0
     if ((write == 1) and (left > 0))
       # Since we can't write "tenty-two" instead of "twelve",
       # we have to make a special exception for these.
       numString = numString + teenagers[left-1]
-      # The "-1" is because teenagers[3] is 'fourteen', not 'thirteen'.
-
-      # Since we took care of the digit in the ones place already,
-      # we have nothing left to write.
       left = 0
     else
       numString = numString + tensPlace[write-1]
-      # The "-1" is because tensPlace[3] is 'forty', not 'thirty'.
     end
-
     if left > 0
-      # So we don't write 'sixtyfour'...
       numString = numString + '-'
     end
   end
 
-  write = left  # How many ones left to write out?
-  left  = 0     # Subtract off those ones.
-
+  write = left
+  left  = 0
   if write > 0
     numString = numString + onesPlace[write-1]
-    # The "-1" is because onesPlace[3] is 'four', not 'three'.
   end
-
-  # Now we just return "numString"...
+  
   numString
 end
 
@@ -108,7 +69,6 @@ puts englishNumber( 99)
 puts englishNumber(100)
 puts englishNumber(101)
 puts englishNumber(234)
-puts englishNumber(1999)
-puts englishNumber(3211)
-puts englishNumber(999999)
-puts englishNumber(1000000000000)
+puts englishNumber(3_211)
+puts englishNumber(999_999)
+puts englishNumber(1_000_000_000_000)
